8817669ed2782d8c545ea95c7e628767e9b7ebc6
Joel <jdeanjj1000@gmail.com>
2015-11-10 15:06:22 -0800

    started packaging structure. About to add dependency injection

--- app/src/main/java/com/mentor/ui/MainActivity.java
+++ app/src/main/java/com/mentor/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.mentor;
+package com.mentor.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.mentor.R;
 
 public class MainActivity extends AppCompatActivity {
 
