1b9aabdc5a3a0ab4d55048bf1c460269762b48ab
Joel <jdeanjj1000@gmail.com>
2015-11-10 00:13:45 -0800

    added several libs that will be used to create quick prototype

---
+++ app/src/main/java/com/mentor/MentorApp.java
@@ -0,0 +1,14 @@
+package com.mentor;
+
+import android.app.Application;
+
+/**
+ * Created by Joel on 09/11/2015.
+ */
+public class MentorApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
