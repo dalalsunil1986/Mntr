db6515610f644f39dd1c6226508ad491fc3516e1
Joel <jdeanjj1000@gmail.com>
2015-11-11 16:24:51 -0800

    configured gcm sender id

--- app/src/main/java/com/mentor/MentorApp.java
+++ app/src/main/java/com/mentor/MentorApp.java
@@ -1,14 +1,31 @@
 package com.mentor;
 
 import android.app.Application;
+import android.os.Bundle;
+
+import net.danlew.android.joda.JodaTimeAndroid;
+
+import eu.inloop.easygcm.GcmListener;
 
 /**
  * Created by Joel on 09/11/2015.
  */
-public class MentorApp extends Application {
+public class MentorApp extends Application implements GcmListener {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        JodaTimeAndroid.init(this);
+    }
+
+    @Override
+    public void onMessage(String s, Bundle bundle) {
+
+    }
+
+    @Override
+    public void sendRegistrationIdToBackend(String s) {
+
     }
 }
