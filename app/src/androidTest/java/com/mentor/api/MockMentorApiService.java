1a4cac058c81f4ffdd40fe52e23c4449907179cc
Joel <jdeanjj1000@gmail.com>
2015-12-14 17:15:58 -0500

    start wakie list ui

--- app/src/androidTest/java/com/mentor/api/MockMentorApiService.java
+++ app/src/androidTest/java/com/mentor/api/MockMentorApiService.java
@@ -1,10 +1,19 @@
 package com.mentor.api;
 
 import com.mentor.api.MentorApiService;
+import com.mentor.api.models.CreateWakieModel;
+import com.mentor.api.models.GetWakieModel;
+
+import retrofit.Call;
+import retrofit.http.Body;
 
 /**
  * Created by Joel on 23/11/2015.
  */
 public class MockMentorApiService implements MentorApiService{
 
+    @Override
+    public Call<GetWakieModel> createWakie(@Body CreateWakieModel wakieModel) {
+        return null;
+    }
 }
